2579c3dd52d1cb20f39369b2e64c74a3eb7c321e
javisaravia <fso0001@alu.medac.es>
2025-11-10 12:22:19 +0100

    Add files via upload

--- taskflow/app/functions.php
+++ taskflow/app/functions.php
@@ -1,9 +1,7 @@
-/* */
 <?php
 
-function obtenerClasePrioridad($prioridad)
+function obtenerClasePrioridad(string $prioridad): string
 {
-  
     switch ($prioridad) {
         case 'alta':
             return 'prioridad-alta';
@@ -16,15 +14,20 @@ function obtenerClasePrioridad($prioridad)
     }
 }
 
-function renderizarTarea($tarea)
+function renderizarTarea(array $tarea): string
 {
-    $clasePrioridad = obtenerClasePrioridad($tarea['prioridad']);
+    // defensivamente usamos valores por defecto para evitar warnings si faltan índices
+    $prioridad = $tarea['prioridad'] ?? '';
+    $completado = $tarea['completado'] ?? false;
+    $titulo = $tarea['titulo'] ?? '';
 
-    $claseCompletada = $tarea['completado'] ? 'completada' : '';
-
+    $clasePrioridad = obtenerClasePrioridad($prioridad);
+    $claseCompletada = $completado ? 'completada' : '';
     $clasesCSS = trim($clasePrioridad . ' ' . $claseCompletada);
+
+    // sanitizamos el título para evitar inyección de HTML
+    $tituloSeguro = htmlspecialchars($titulo, ENT_QUOTES, 'UTF-8');
+
+    return "<li class='{$clasesCSS}'>{$tituloSeguro}</li>";
 }
-return "<li class='{$clasesCSS}'>{$tarea['titulo']}</li>"; 
-
-
 ?>
